e7ab54a36e804e0885155888330f8fc3c0618026
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-07-01 13:25:37 -0400

    Add Function to Place X or O in Cell

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const X_CLASS = "x";
+const CIRCLE_CLASS = "circle";
+
 const cellElements = document.querySelectorAll("[data-cell]");
+
+let circleTurn;
 
 cellElements.forEach(cell => {
     // only fire click event ONCE
@@ -6,8 +11,19 @@ cellElements.forEach(cell => {
 });
 
 function handleClick(e) {
+    // get cell that was clicked on
+    const cell = e.target;
+
+    // determine whether to apply circle class or x class
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+
     // place mark
+    placeMark(cell, currentClass)
     // check for win
     // check for draw
     // switch turns
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
 }
